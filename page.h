af770e9f450932e960d4b540116ed221a40e636c
eevee <vehling@usc.edu>
2019-08-05 10:36:14 -0700

    Add files via upload

---
+++ page.h
@@ -0,0 +1,14 @@
+#ifndef PAGE_H
+#define PAGE_H
+#include <string>
+using std::string;
+#include<vector>
+using std::vector;
+class Page { 
+public:
+  Page();
+  //setters and getters
+private:
+  //Data members: id, url, pagerank, links , .. etc
+};  
+#endif
